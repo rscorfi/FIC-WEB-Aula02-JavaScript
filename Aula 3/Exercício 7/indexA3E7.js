7b7485e0bf1f7331af5c3bd82c4e12de75e94199
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-05-12 20:03:00 -0300

    Criado A3E7

---
+++ Aula 3/Exercício 7/indexA3E7.js
@@ -0,0 +1,9 @@
+let nomeFuncionario = 'Giuseppe Kadura'
+let cargoFuncionario = 'Lavador de carros'
+let tempoEmpresaFuncionario = 68
+let ativoFuncionario = true
+
+console.log('Nome do colaborador:' , nomeFuncionario)
+console.log('Cargo:' , cargoFuncionario)
+console.log('Tempo de empresa:' , tempoEmpresaFuncionario , 'anos')
+console.log('Colaborador Ativo?' , ativoFuncionario)
