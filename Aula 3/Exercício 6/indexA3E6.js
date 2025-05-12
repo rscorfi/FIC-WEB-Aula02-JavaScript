0e142352bfd7311a0dc9ff136a62b32b2144660e
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-05-12 19:47:12 -0300

    Criado Exercício 6

---
+++ Aula 3/Exercício 6/indexA3E6.js
@@ -0,0 +1,9 @@
+let filme_titulo = 'O Senhor dos Anéls: A Saciedade dos Profetas Mortos'
+let filme_ano = 1841
+let filme_duracao = 68
+let filme_maior18 = false
+
+console.log('Título:' , filme_titulo)
+console.log('Ano de lançamento:' , filme_ano)
+console.log('Duração:' , filme_duracao , 'min.')
+console.log('Classificado para maiores de 1,8 décadas:' , filme_maior18)
