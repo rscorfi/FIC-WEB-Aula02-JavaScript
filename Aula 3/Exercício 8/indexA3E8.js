b8285bf27324a7d156896aa5d95bec442343dc0d
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-05-12 20:12:32 -0300

    Alterado A3E8, criado A3E9

--- Aula 3/Exercício 8/indexA3E8.js
+++ Aula 3/Exercício 8/indexA3E8.js
@@ -1,6 +1,4 @@
-let nome = 'Joilson'
-let sobrenome = 'Mendes'
 let idade = 42
 let idade_str = String (idade)
 
-console.log('Meu nome é ' + nome + ' ' + sobrenome + '. Eu tenho ' + idade_str + ' anos')
+console.log('Eu tenho ' + idade_str + ' anos')
---
+++ Aula 3/Exercício 9/indexA3E9.js
@@ -0,0 +1,5 @@
+let nome = 'Joilson'
+let sobrenome = 'Mendes'
+let idade = 42
+
+console.log('Meu nome é ' + nome + ' ' + sobrenome + '. Eu tenho ' + idade + ' anos')
